c43ccc83492ea003c57a7e0e49ef7927aded227b
NouraNou <oneway.33@hotmail.com>
2023-05-28 18:53:08 +0300

    task1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import bakery2 from "./asssets/image/bakery2.jpg";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1> Fraesh bun Bekery</h1>
+      <p>place to start your morning</p>
+      <img
+        src="https://www.expedit.eu/media/11173/bakery-solutions03.jpg?mode=crop&heightratio=0.4845360824742268041237113402&width=1110"
+        className="bakery2"
+      />
+    </>
   );
 }
 
